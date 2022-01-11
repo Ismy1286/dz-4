d8c43b2cb8f9d95c902598805c7cd3aaac354203
Ainur <ismy1286@gmail.com>
2022-01-11 13:12:12 +0600

    first commit

---
+++ DZ-4.5.4/NewsCell.swift
@@ -0,0 +1,8 @@
+//
+//  NewsCell.swift
+//  DZ-4.5.4
+//
+//  Created by user on 10.01.2022.
+//
+
+import Foundation
---
+++ DZ-4.5.4/NewsModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewsModel.swift
+//  DZ-4.5.4
+//
+//  Created by user on 10.01.2022.
+//
+
+import Foundation
